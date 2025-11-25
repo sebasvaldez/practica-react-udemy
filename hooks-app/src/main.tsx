d351a8e7fb11e776fd0ecd1abbfa356e3b3600e3
Marcelo Sebastián Valdez <48698148+sebasvaldez@users.noreply.github.com>
2025-11-24 21:04:55 -0300

    combinando multiples hooks

--- hooks-app/src/main.tsx
+++ hooks-app/src/main.tsx
@@ -3,12 +3,14 @@ import { createRoot } from "react-dom/client";
 // import { HooksApp } from "./HooksApp";
 // import { TrafficLight } from "./01-useState/TrafficLight";
 // import { TrafficLightWithEffect } from "./02-useEffect/TrafficLightWithEffect";
-import { TrafficLightWithEffectWithHook } from "./02-useEffect/TrafficLightWithEffectWithHook";
+// import { TrafficLightWithEffectWithHook } from "./02-useEffect/TrafficLightWithEffectWithHook";
 
 import "./index.css";
+import { PokemonPage } from "./03-examples/PokemonPage";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <TrafficLightWithEffectWithHook />
+    {/* <TrafficLightWithEffectWithHook /> */}
+    <PokemonPage />
   </StrictMode>
 );
